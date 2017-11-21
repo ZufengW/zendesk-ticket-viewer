fae207ee9486b0503468d145f7b15dd3d4cf781a
Zufeng W <ZufengW@gmail.com>
2017-11-21 14:33:39 +1100

    Add user input
    User can type "1" to display a single ticket or "2" to display all tickets.

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 var Zendesk = require('zendesk-node-api');
 var config = require('./config.json');
+const readline = require('readline');
 
 var zendesk = new Zendesk(config);
 const H_RULE = '---------------------------------------------------------';
 
+var rl = readline.createInterface(process.stdin, process.stdout);
 
-displayAllTickets();
-displayTicketInfoWithID(1);
-displayTicketInfoWithID(2);
+rl.setPrompt('> ');
+rl.prompt();
+
+// listen for user input
+rl.on('line', function(line) {
+  var input = line.trim().split(/\s+/);
+  switch(input[0]) {
+    case '1':
+      // display a single ticket
+      if (input.length < 2) {
+        console.log("Usage is > 1 id");
+        rl.prompt();
+      } else {
+        rl.pause();
+        console.log(`Searching for ticket with id ${input[1]}...`);
+        displayTicketInfoWithID(input[1]);
+      }
+      break;
+    case '2':
+      // display all tickets
+      rl.pause();
+      console.log(`Displaying all tickets...`);
+      displayAllTickets();
+      break;
+    default:
+      // user input didn't match anything
+      console.log("1 or 2");
+      rl.prompt();
+      break;
+  }
+}).on('close', function() {
+  console.log('Complete.');
+  process.exit(0);
+});
 
 
 /** displays a single ticket by id */
@@ -16,23 +49,31 @@ function displayTicketInfoWithID(id) {
     console.log(H_RULE);
     displayTicketInfo(result);
     console.log(H_RULE);
+    rl.prompt();
   }, function(reason) {
     displayRejectionReason(reason);
+    rl.prompt();
   });
 }
 
 
-/** displays all the tickets of zendesk */
+/** displays all the tickets of Zendesk */
 function displayAllTickets() {
   zendesk.tickets.list().then(function(result) {
     console.log(H_RULE);
-    for (ticket of result){
-      displayTicketInfo(ticket);
-      console.log('');
+    if (result){
+      for (ticket of result){
+        displayTicketInfo(ticket);
+        console.log('');
+      }
+    } else {
+      console.log('No tickets found');
     }
     console.log(H_RULE);
+    rl.prompt();
   }, function(reason) {
     displayRejectionReason(reason);
+    rl.prompt();
   });
 }
 
